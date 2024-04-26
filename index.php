8e9ba573004641a4971be2b7edb9adc49cefacdd
Endoman3301 <99251665+Ndoman3301@users.noreply.github.com>
2024-04-26 21:04:03 +0200

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<php 
+$ahoj = "My website wink";
+?>
+
 <!DOCTYPE html>
 <html lang="cs">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Beautiful Website, wink</title>
+    <title><php $ahoj ?></title>
 </head>
 <body>
     <h1>Welcome on this page!</h1>
